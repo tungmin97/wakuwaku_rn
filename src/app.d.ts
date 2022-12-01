5490b2e0917ef632a3244dd2be3a9d31a5c202a7
tungmin97 <tungmin97@gmail.com>
2022-12-01 10:52:32 +0700

    actually done with schedule screen

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="nativewind/types" />
+declare module 'react-native-vector-icons/dist/Entypo';
+declare module 'react-native-vector-icons/dist/MaterialIcons';
+declare module 'react-native-vector-icons/dist/Ionicons';
--- src/components/AnimeCard/AnimeCard.tsx
+++ src/components/AnimeCard/AnimeCard.tsx
@@ -1,9 +1,10 @@
 import { TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { AnimeById } from '@src/types/animeTypes';
 import FastImage from 'react-native-fast-image';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackNavigationProps } from '@src/types/types';
+import HomeModal from '@components/HomeModal/HomeModal';
 
 interface Props {
   item: AnimeById;
@@ -13,14 +14,27 @@ const AnimeCard = ({ item }: Props) => {
   const navigation = useNavigation<RootStackNavigationProps>();
   const handleNavigation = () => navigation.navigate('Details', { item: item });
 
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const handleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   return (
-    <TouchableOpacity activeOpacity={0.8} className="p-1.5" onPress={handleNavigation}>
-      <FastImage
-        resizeMode={FastImage.resizeMode.cover}
-        source={{ uri: item.images.jpg.image_url }}
-        className="rounded-md w-44 h-64 justify-end bg-white"
+    <>
+      <TouchableOpacity activeOpacity={0.8} className="p-1.5" onPress={handleModal}>
+        <FastImage
+          resizeMode={FastImage.resizeMode.cover}
+          source={{ uri: item.images.jpg.image_url }}
+          className="rounded-md w-44 h-64 justify-end bg-white"
+        />
+      </TouchableOpacity>
+      <HomeModal
+        visible={isModalVisible}
+        item={item}
+        handleNavigation={handleNavigation}
+        handleModal={handleModal}
       />
-    </TouchableOpacity>
+    </>
   );
 };
 
